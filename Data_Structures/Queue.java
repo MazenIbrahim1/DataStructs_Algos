295c67f1fc490b6ebe1843028b7b073863883d82
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-01-21 17:21:31 -0500

    Starting queue and stack

--- Data_Structures/Queue.java
+++ Data_Structures/Queue.java
@@ -1,5 +1,6 @@
 package Data_Structures;
 
+// FIFO First In First Out
 public class Queue {
     
 }
--- Data_Structures/Stack.java
+++ Data_Structures/Stack.java
@@ -1,5 +1,6 @@
 package Data_Structures;
 
+// LIFO --> Last In First Out
 public class Stack {
     
 }
