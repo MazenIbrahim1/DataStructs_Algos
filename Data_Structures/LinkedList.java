3e6a75cac222d4f45e4efc2cf35439ee13198420
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-01-21 13:34:14 -0500

    node class for linkedlist

--- Data_Structures/LinkedList.java
+++ Data_Structures/LinkedList.java
@@ -1,5 +1,46 @@
 package Data_Structures;
 
-public class LinkedList {
+// Doubly LinkedList
+public class LinkedList<T> {
+    // Nested Node Class
+    public class Node {
+        private T data;
+        private Node next;
+        private Node prev;
+
+        // Constructor
+        public Node() {
+            this.data = null;
+            this.next = null;
+            this.prev = null;
+        }
+
+        // Getters 
+        public T getData() {
+            return data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        // Setters
+        public void setData(T newData) {
+            data = newData;
+        }
+
+        public void setNext(Node newNext) {
+            next = newNext;
+        }
+
+        public void setPrev(Node newPrev) {
+            prev = newPrev;
+        }
+    }
+
     
 }
