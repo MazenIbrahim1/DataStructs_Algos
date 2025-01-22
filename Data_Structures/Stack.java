27c5fb892907fcebe70f733675341398b6694ea1
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-01-21 19:33:04 -0500

    stack outline

--- Data_Structures/Stack.java
+++ Data_Structures/Stack.java
@@ -1,6 +1,38 @@
 package Data_Structures;
 
 // LIFO --> Last In First Out
-public class Stack {
-    
+public class Stack<T> {
+    private int size;
+
+    public Stack() {
+        this.size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void push(T item) {
+
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int search(T item) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
