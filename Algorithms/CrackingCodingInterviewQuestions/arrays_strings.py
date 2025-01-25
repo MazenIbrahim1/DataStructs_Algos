b8df77940843b4281f0df5625d546ec4407b00b9
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-01-24 23:12:28 -0500

    more questions

--- Algorithms/CrackingCodingInterviewQuestions/arrays_strings.py
+++ Algorithms/CrackingCodingInterviewQuestions/arrays_strings.py
@@ -3,3 +3,20 @@
 # 1.1 -- Is Unique
 # Implement an algorithm to determine if a string has all unique characters. What if you
 # cannot use additional data structures?
+def isUnique(string):
+    pass
+
+
+# 1.2 -- Check Permutations
+# Given two strings, write a method to determine if one is a permutation of the other.
+def isPermutation(string1, string2):
+    pass
+
+
+# 1.3 -- URLify 
+# Write a method to replace all spaces in a string with '%20'. You may assume that the string
+# has sufficient space at the end to hold the additional characters, and that you are given the "true"
+# length of the string. (Note: If implementing in Java, please use a character array so that you can
+# perform this operation in place.)
+def URLify(string):
+    pass
