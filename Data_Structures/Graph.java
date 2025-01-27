9f649b40deef3d18adac3be96216cbd5be5251b2
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-01-26 22:26:42 -0500

    adding vertex and edges in graph

--- Data_Structures/Graph.java
+++ Data_Structures/Graph.java
@@ -13,8 +13,24 @@ public class Graph<T> {
         map.put(vertex, new LinkedList<>());
     }
 
-    public void addEdge() {
+    public void addEdge(T source, T destination, boolean bidirectional) {
+        if (!map.containsKey(source)) {
+            addVertex(source);
+        }
 
+        if (!map.containsKey(destination)) {
+            addVertex(destination);
+        }
+
+        map.get(source).add(destination);
+        if (bidirectional) {
+            map.get(destination).add(source);
+        }
+    }
+
+    // Making the default value of bidirectional = true
+    public void addEdge(T source, T destination) {
+        addEdge(source, destination, true);
     }
 
     public static void main(String[] args) {
