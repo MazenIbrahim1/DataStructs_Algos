3d32773e11a22bb2418477a2c3f7c2002b0a984b
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-01-29 23:57:41 -0500

    urlify

--- Algorithms/CrackingCodingInterviewQuestions/arrays_strings.py
+++ Algorithms/CrackingCodingInterviewQuestions/arrays_strings.py
@@ -3,6 +3,9 @@
 # 1.1 -- Is Unique
 # Implement an algorithm to determine if a string has all unique characters. What if you
 # cannot use additional data structures?
+from typing import Optional
+
+
 def isUnique(string):
     # Time and Space --> O(n)
     return len(string) == len(set(string))
@@ -35,10 +38,9 @@ def isPermutation2(string1, string2):
 # 1.3 -- URLify 
 # Write a method to replace all spaces in a string with '%20'. You may assume that the string
 # has sufficient space at the end to hold the additional characters, and that you are given the "true"
-# length of the string. (Note: If implementing in Java, please use a character array so that you can
-# perform this operation in place.)
-def URLify(string):
-    pass
+# length of the string. 
+def URLify(string: str):
+    return string.replace(" ", "%20")
 
 
 # Main 
