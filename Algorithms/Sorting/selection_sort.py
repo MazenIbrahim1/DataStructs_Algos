eeb1a38fffa37c6c4bf483ae8d2e3a71fc783c4b
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-05 19:55:10 -0500

    selection sort

--- Algorithms/Sorting/selection_sort.py
+++ Algorithms/Sorting/selection_sort.py
@@ -2,4 +2,21 @@
 # Time Complexity: O(n^2)
 
 def selection_sort(lst: list):
-    pass
+    curr_min_index = 0
+
+    for i in range(len(lst)):
+        curr_min_index = i
+        for j in range(i+1 ,len(lst)):
+            if lst[j] < lst[curr_min_index]:
+                curr_min_index = j
+        if curr_min_index != i:
+            # Swap elements
+            lst[i], lst[curr_min_index] = lst[curr_min_index], lst[i]
+
+# Test
+arr = [12, 11, 13, 5, 6]
+arr2 = [2,8,5,3,9,4,1]
+selection_sort(arr)
+selection_sort(arr2)
+print(arr)
+print(arr2)
