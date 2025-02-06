615e220c2f107dc180328f8781b937c65cbbeefa
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-05 19:55:51 -0500

    test for sorts

--- Algorithms/Sorting/merge_sort.py
+++ Algorithms/Sorting/merge_sort.py
@@ -3,3 +3,11 @@
 
 def merge_sort(lst):
     pass
+
+# Test
+arr = [12, 11, 13, 5, 6]
+arr2 = [2,8,5,3,9,4,1]
+merge_sort(arr)
+merge_sort(arr2)
+print(arr)
+print(arr2)
--- Algorithms/Sorting/quick_sort.py
+++ Algorithms/Sorting/quick_sort.py
@@ -3,3 +3,11 @@
 
 def quick_sort(lst):
     pass
+
+# Test
+arr = [12, 11, 13, 5, 6]
+arr2 = [2,8,5,3,9,4,1]
+quick_sort(arr)
+quick_sort(arr2)
+print(arr)
+print(arr2)
