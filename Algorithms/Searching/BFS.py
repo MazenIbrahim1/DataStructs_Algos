766bdf705c9e536b3f6925a55e4b51020467026c
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-09 18:52:52 -0500

    started bfs

--- Algorithms/Searching/BFS.py
+++ Algorithms/Searching/BFS.py
@@ -1,4 +1,14 @@
 # Breadth First Search
 # O(V + E)
-def bfs(node):
-    pass
+def bfs(node) -> None:
+    if node is None:
+        return
+    
+    visited = set()
+    queue = []
+
+    visited.add(node)
+    queue.append(node)
+
+    while len(queue) != 0:
+        pass
