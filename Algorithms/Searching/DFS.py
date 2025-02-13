843f037bb1d82273f4f2c1c1b2ad7bc45ecad1b7
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-12 23:45:31 -0500

    dfs

--- Algorithms/Searching/DFS.py
+++ Algorithms/Searching/DFS.py
@@ -1,4 +1,14 @@
 # Depth First Search
 # O(V + E)
 def dfs(node):
-    pass
+    if node is None:
+        return
+    
+    visited = set()
+    stack = []
+
+    visited.add(node)
+    stack.append(node)
+
+    while len(stack) != 0:
+        pass
