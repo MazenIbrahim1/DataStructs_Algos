d21b7ad1bb17255d6040648d983ae4e65606c5af
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-16 13:08:51 -0500

    binary search

--- Algorithms/Searching/binary_search.py
+++ Algorithms/Searching/binary_search.py
@@ -1,4 +1,13 @@
 # Binary Search Algorithm
 
 def binary_search(arr, target):
-    pass
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
