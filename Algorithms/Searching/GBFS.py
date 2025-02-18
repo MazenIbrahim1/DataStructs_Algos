b14ddd7849e2d76acfc340ac173920dc21075f0c
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-18 12:57:12 -0500

    heuristic function for gfs and a*

--- Algorithms/Searching/GBFS.py
+++ Algorithms/Searching/GBFS.py
@@ -1,4 +1,8 @@
-# Greedy best-first search
+# Greedy best-first search with manhattan distance as the heuristic function
+import heapq
+
+def mahattan_distance(x, y):
+    return abs(x[0] - y[0]) + abs(x[1] - y[1])
 
 def gbfs(node):
     pass
--- Algorithms/Searching/astar.py
+++ Algorithms/Searching/astar.py
@@ -1,2 +1,8 @@
+# A* search with manhattan distance as the heuristic function
+import heapq
+
+def mahattan_distance(x, y):
+    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+
 def aStar(node):
     pass
