accf4f0391789de40216cfe75029093ecebf6d9f
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-17 19:19:02 -0500

    questions

--- Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
+++ Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
@@ -1,5 +1,16 @@
 # Questions 4.1 - 4.12
 
+# 4.1 Route between nodes
+# Given a directed graph, design an algorithm to find out whether there is a
+# route between two nodes.
+def route(graph, node1, node2):
+    pass
+
+# 4.2 Minimal Tree
+# Given a sorted (increasing order) array with unique integer elements, write an algorithm
+# to create a binary search tree with minimal height.
+def minimal_tree(arr):
+    pass
 
 # Main
 def main():
