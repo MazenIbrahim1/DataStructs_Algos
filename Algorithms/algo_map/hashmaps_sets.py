90f201491beed4c4a8e549fbf9e143a0b06769cc
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-20 20:43:39 -0500

    algo map

--- Algorithms/algo_map/hashmaps_sets.py
+++ Algorithms/algo_map/hashmaps_sets.py
@@ -1,2 +1,11 @@
 # Algo map leetcode questions
 from typing import List
+
+# Jewels and Stones
+def numJewelsInStones(jewels: str, stones: str) -> int:
+    pass
+
+# Contains Duplicate
+def containsDuplicate(nums: List[int]) -> bool:
+    num_set = set(nums)
+    return len(num_set) != len(nums)
