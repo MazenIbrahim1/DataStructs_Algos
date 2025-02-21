e541cdd5eabc0500836051d2f2cea16095276b9e
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-21 11:53:08 -0500

    stack

--- Algorithms/algo_map/stacks.py
+++ Algorithms/algo_map/stacks.py
@@ -24,3 +24,20 @@ def calPoints(operations: List[str]) -> int:
             score += int(operation)
     
     return score
+
+# Valid Parentheses
+def isValid(s: str) -> bool:
+    stack = []
+
+    for char in s:
+        if char in "({[":
+            stack.append(char)
+        else:
+            if not stack or \
+                (char == ')' and stack[-1] != '(') or \
+                (char == '}' and stack[-1] != '{') or \
+                (char == ']' and stack[-1] != '['):
+                    return False
+            stack.pop()
+    
+    return not stack
