526e18c35e217655fd6bb69254b869e6660364e2
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-21 22:33:38 -0500

    time and space

--- Algorithms/algo_map/linked_list.py
+++ Algorithms/algo_map/linked_list.py
@@ -1,2 +1,22 @@
 # Algo map leetcode questions
-from typing import List
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Delete Duplicates
+def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    cursor = head
+
+    while cursor and cursor.next:
+        if cursor.val == cursor.next.val:
+            cursor.next = cursor.next.next
+        else:
+            cursor = cursor.next
+    
+    return head
