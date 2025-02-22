4fc3f594575e1d958d86754671b8e52c30ecd493
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-22 13:09:09 -0500

    summary ranges

--- Algorithms/algo_map/arrays_strings.py
+++ Algorithms/algo_map/arrays_strings.py
@@ -84,3 +84,27 @@ def longestCommonPrefix(strs: List[str]) -> str:
         i += 1
 
     return s[:i]
+
+# Summary Ranges
+def summaryRanges(nums: List[int]) -> List[str]:
+    # Time and Space complexity: O(n)
+    if len(nums) == 1:
+        return [f"{nums[0]}"]
+
+    ans = []
+    i = 0
+
+    while i < len(nums):
+        start = nums[i]
+
+        while i < len(nums) - 1 and nums[i] + 1 == nums[i+1]:
+            i += 1
+
+        if start != nums[i]:
+            ans.append(str(start) + '->' + str(nums[i]))
+        else:
+            ans.append(str(nums[i]))
+        
+        i += 1
+    
+    return ans
