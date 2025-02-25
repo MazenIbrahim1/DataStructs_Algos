9e77c9a263b4dc6236263d5a6aec61cd314c51ca
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-24 20:35:02 -0500

    addition

--- Algorithms/algo_map/hashmaps_sets.py
+++ Algorithms/algo_map/hashmaps_sets.py
@@ -3,7 +3,16 @@ from typing import List
 
 # Jewels and Stones
 def numJewelsInStones(jewels: str, stones: str) -> int:
-    pass
+    # Time complexity = O(J + S)
+    # Space complexity = O(J)
+    s = set(jewels)
+    output = 0
+
+    for char in s:
+        if char in stones:
+            output += 1
+    
+    return output
 
 # Contains Duplicate
 def containsDuplicate(nums: List[int]) -> bool:
