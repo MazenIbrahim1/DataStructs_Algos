5ae958bc28b50073bd09fbbff92adc3288a5c5a1
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-28 12:06:03 -0500

    binary search

--- Algorithms/algo_map/binary_search.py
+++ Algorithms/algo_map/binary_search.py
@@ -1,2 +1,20 @@
 # Algo map leetcode questions
 from typing import List
+
+# Binary Search
+def binary_search(nums: List[int], target: int) -> int:
+    # O(log n)
+    if not nums: return -1
+
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return -1
