4165d1b7aaf202e1e76108a239f1a5060e9184e7
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-02-28 23:01:35 -0500

    2 pointer

--- Algorithms/algo_map/2_pointers.py
+++ Algorithms/algo_map/2_pointers.py
@@ -1,2 +1,14 @@
 # Algo map leetcode questions
 from typing import List
+
+# Reverse a string
+def reverseString(s: List[str]) -> None:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -=1
