828e6618c8797c62297020a0893f0fef81427e63
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-07 23:00:46 -0500

    trees

--- Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
+++ Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
@@ -18,6 +18,13 @@ def minimal_tree(arr):
 def list_of_depths(tree):
     pass
 
+# 4.4 Check Balanced
+# Implement a function to check if a binary tree is balanced. For the purposes of
+# this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any
+# node never differ by more than one.
+def check_balanced(tree):
+    pass
+
 # Main
 def main():
     pass
