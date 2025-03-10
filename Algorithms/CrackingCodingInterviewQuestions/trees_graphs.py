7feb09851236d6a680613a84b5b9ee4a0b8c2f00
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-10 11:35:16 -0400

    trees

--- Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
+++ Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
@@ -30,6 +30,12 @@ def check_balanced(tree):
 def validBST(tree):
     pass
 
+# 4.6 Successor
+# Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
+# binary search tree. You may assume that each node has a link to its parent.
+def successor(tree, node):
+    pass
+
 # Main
 def main():
     pass
