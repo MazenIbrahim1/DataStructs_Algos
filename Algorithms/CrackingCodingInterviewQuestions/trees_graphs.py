e5b33fc00f5e1137f2fea726eae4c6385d008cfd
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-31 18:54:16 -0400

    list of depths

--- Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
+++ Algorithms/CrackingCodingInterviewQuestions/trees_graphs.py
@@ -46,7 +46,24 @@ def minimal_tree(arr):
 # Given a binary tree, design an algorithm which creates a linked list of all the nodes
 # at each depth
 def list_of_depths(tree):
-    pass
+    if not tree:
+        return []
+    
+    result = []
+    queue = deque([(tree, 0)])
+    
+    while queue:
+        node, depth = queue.popleft()
+        if len(result) <= depth:
+            result.append([])
+        result[depth].append(node.val)
+        
+        if node.left:
+            queue.append((node.left, depth + 1))
+        if node.right:
+            queue.append((node.right, depth + 1))
+    
+    return result
 
 # 4.4 Check Balanced
 # Implement a function to check if a binary tree is balanced. For the purposes of
